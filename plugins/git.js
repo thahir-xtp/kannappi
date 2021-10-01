6801ab59bf9ea6bd1ef44da94d9816cb04a4ff1b
A4edits <90356972+A4edits@users.noreply.github.com>
2021-10-01 18:19:22 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
+  <img src="A4edits.jpg"  width="200" height="200"/>
     
     
     var i = Math.floor(1*Math.random())
