254b12b1892ae36439215ecd116bc96b04685b96
A4edits <90356972+A4edits@users.noreply.github.com>
2021-10-03 02:14:31 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -44,7 +44,7 @@ ${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
         }
         else {
             await conn.sendMessage(conn.user.jid,st, MessageType.text)
-            await conn.sendMessage(conn.user.jid,'*Do Not Share This Code With Anyone!* ```thank you for choosing sushilan by a4 edits```', MessageType.text)
+            await conn.sendMessage(conn.user.jid,'*Do Not Share This Code With Anyone!```thank you for choosing sushilan by a4 edits```*', MessageType.text)
             console.log(
                 chalk.blue.bold('If you are installing locale, you can start the bot with node bot.js')
             );
